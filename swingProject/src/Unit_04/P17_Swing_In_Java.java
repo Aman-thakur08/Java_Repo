2e2ba954d06d6de2a722ed9906a1345e314885aa
Aman-thakur08 <amant1197@gmail.com>
2022-06-02 13:19:28 +0530

    gui

---
+++ swingProject/src/Unit_04/P17_Swing_In_Java.java
@@ -0,0 +1,73 @@
+package Unit_04;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+//create template
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class P17_Swing_In_Java {
+
+	public static void main(String[] args) {
+		SwingInJava name=new SwingInJava();
+
+	}
+
+}
+class SwingInJava extends JFrame{
+	JTextArea ta1;
+	JTextField t1;
+	JTextField t2;
+	JButton b1;
+	JButton b2;
+	JButton b3;
+	JButton b4;
+	
+	JLabel l1;
+	
+	SwingInJava(){
+		ta1 = new JTextArea(10,30); 
+		t1=new JTextField(20);
+		t2=new JTextField(20);
+		
+		b1=new JButton("Add");
+		b2=new JButton("Subtract");
+		b3=new JButton("Multiply");
+		b4=new JButton("Divide");
+		l1=new JLabel("Res");
+		add(ta1);
+		add(t1);
+		add(t2);
+		System.out.println();
+		add(b1);
+		add(b2);
+		add(b3);
+		add(b4);
+		add(l1);
+		ActionListener al =new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String s1=t1.getText();
+				String s2=t2.getText();
+				int a1=Integer.parseInt(s1);
+				int a2=Integer.parseInt(s2);
+				
+				
+			}
+		};
+		b1.addActionListener(al);
+		b2.addActionListener(al);
+		b3.addActionListener(al);
+		b4.addActionListener(al);
+		setLayout(new FlowLayout());
+		setVisible(true);
+		setSize(400,400);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
